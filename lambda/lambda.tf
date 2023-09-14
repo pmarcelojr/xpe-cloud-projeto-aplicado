607fb4acca1cbf8fd16ebe4ca1eeb3c5cc18637e
pmarcelojr <marcelosantostecnologia@gmail.com>
2023-09-13 22:28:27 -0300

    ajuste code pacote lambda

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "lambda_function_trigger" {
 
   filename = "lambdatrigger.zip"
   # source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
-  source_code_hash = filebase64sha256("./code/function.zip")
+  source_code_hash = filebase64sha256("../lambda/code/function.zip")
   role             = aws_iam_role.lambda_role.arn
   runtime          = "python3.6"
   handler          = "lambda_function.lambda_handler"
