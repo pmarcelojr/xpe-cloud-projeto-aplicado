d4bcb9e0f7f6320d0320e5f1dc2dd51c53c34221
pmarcelojr <marcelosantostecnologia@gmail.com>
2023-09-13 22:26:59 -0300

    ajustes pacote lambda

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -1,11 +1,11 @@
 /* 
 Arquive the script
 */
-data "archive_file" "python_lambda_package" {
-  type        = "zip"
-  source_file = "./code/function.zip"
-  # output_path = "lambdatrigger.zip"
-}
+# data "archive_file" "python_lambda_package" {
+#   type        = "zip"
+#   source_file = "./code/function.zip"
+#   # output_path = "lambdatrigger.zip"
+# }
 
 /*
   Create the lamda function
@@ -13,8 +13,9 @@ data "archive_file" "python_lambda_package" {
 resource "aws_lambda_function" "lambda_function_trigger" {
   function_name = "cargamf-trigger-lambda-banco-do-povo"
 
-  filename         = "lambdatrigger.zip"
-  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  filename = "lambdatrigger.zip"
+  # source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  source_code_hash = filebase64sha256("./code/function.zip")
   role             = aws_iam_role.lambda_role.arn
   runtime          = "python3.6"
   handler          = "lambda_function.lambda_handler"
