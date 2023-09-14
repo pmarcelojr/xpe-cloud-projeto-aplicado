f0ce6e877b0b96e519432225656ebe5d9093199f
pmarcelojr <marcelosantostecnologia@gmail.com>
2023-09-14 00:14:02 -0300

    update policy

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -56,24 +56,24 @@ resource "aws_iam_role" "function_role" {
   })
 }
 
-resource "aws_iam_policy" "function_logging_policy" {
-  name = "cargamf-trigger-lambda-logging-policy"
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        Action : [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect : "Allow",
-        Resource : "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
-}
+# resource "aws_iam_policy" "function_logging_policy" {
+#   name = "cargamf-trigger-lambda-logging-policy"
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         Action : [
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ],
+#         Effect : "Allow",
+#         Resource : "arn:aws:logs:*:*:*"
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
   role       = aws_iam_role.function_role.id
-  policy_arn = aws_iam_policy.function_logging_policy.arn
+  policy_arn = "arn:aws:iam::890006230292:policy/cargamf-trigger-lambda-logging-policy"
 }
