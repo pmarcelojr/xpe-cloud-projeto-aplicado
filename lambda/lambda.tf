823fb396460b2d8395c46a2c535cb387d84ed87d
pmarcelojr <marcelosantostecnologia@gmail.com>
2023-09-13 21:53:28 -0300

    ajuste runtime lambda

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -16,7 +16,7 @@ resource "aws_lambda_function" "lambda_function_trigger" {
   filename         = "lambdatrigger.zip"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   role             = aws_iam_role.lambda_role.arn
-  runtime          = "python3.8"
+  runtime          = "python3.6"
   handler          = "lambda_function.lambda_handler"
   timeout          = 120
   memory_size      = 128
